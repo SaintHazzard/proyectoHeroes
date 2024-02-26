0dd8f34239aa107ded430dca15b2ba0830212b47
chamitos1998@hotmail.com <SaintHazzard>
2024-02-26 09:14:45 -0500

    feat: :sparkles: funcion mostrar info
    Se agrega el metodo que muestra al informacion de cada carta

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 
-let mainC = document.querySelector('main')
+let bodyM = document.querySelector('body')
 let dataHeroes;
 const arrayHeroes = []
 class ObjectHeroe {
-  constructor(nombre, descripcion, fecha, foto) {
+  constructor(nombre, descripcion, fecha, foto, cardHtml) {
     this.nombre = nombre
     this.descripcion = descripcion
     this.fecha = fecha
     this.foto = foto
+    this.cardHtml = cardHtml
+
+    this.mostrarInfo = this.mostrarInfo.bind(this);
+
+    this.cardHtml.querySelector(".botonVer").addEventListener('click', this.mostrarInfo);
+  }
+  mostrarInfo() {
+    console.log(`Hola soy ${this.nombre}`);
   }
 }
+
 
 
 
@@ -23,30 +32,39 @@ async function getData() {
   try {
     dataHeroes = await getData();
     for (const keyUniverso in dataHeroes) {
-      let tempContent = document.createElement('div')
+      let tempContent = document.createElement('main')
       tempContent.classList.add(`${keyUniverso}`)
       let logo = document.createElement('div')
+      logo.classList.add("logo")
       let img = document.createElement('img')
-      img.src = `source/${keyUniverso}`
+      img.src = `source/${keyUniverso}.svg`
       logo.appendChild(img)
       tempContent.appendChild(logo)
+      let cajaCartas = document.createElement("div")
+      cajaCartas.classList.add("cards")
       for (const heroe of dataHeroes[keyUniverso]) {
         // arrayHeroes.push(new ObjectHeroe(heroe.nombre, heroe.descripcion, heroe.fecha, heroe.foto))
-        let baseCard = `<div class="card">
+        let baseCard = `
+        <div class="card">
           <div class="imagen">
-            <img src="${heroe.src}" alt="heroe" />
+            <img src="${heroe.foto}" alt="heroe" />
           </div>
-          <div>
+          <div class="info">
             <h2>${heroe.nombre}</h2>
-            <button>Ver</button>
+            <button class="botonVer">Ver</button>
           </div>
         </div>`
-        let tempDivCard = document.createElement('div')
-        tempDivCard.innerHTML = baseCard
-        let nodoCard = tempDivCard.firstChild
-        tempContent.appendChild(nodoCard)
+        let tempDiv = document.createElement('div')
+        // console.log(tempDiv);
+
+        tempDiv.innerHTML = baseCard;
+
+        let nodo = tempDiv.firstElementChild
+        cajaCartas.appendChild(nodo)
+        arrayHeroes.push(new ObjectHeroe(heroe.nombre, heroe.descripcion, heroe.fecha_lanzamiento, heroe.foto, nodo))
       }
-      mainC.appendChild(tempContent)
+      tempContent.appendChild(cajaCartas)
+      bodyM.appendChild(tempContent)
     }
   } catch (error) {
     console.error('Error en la obtención de datos:', error);
